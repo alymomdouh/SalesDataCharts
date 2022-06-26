325dc1846b81b31d76d7cde1c1f921de2fffca1a
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-06-27 00:32:06 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesDataCharts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/SalesEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesDataCharts.Models
+{
+    public class SalesEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MonthName { get; set; }
+        public int TotalSales { get; set; }
+    }
+}
